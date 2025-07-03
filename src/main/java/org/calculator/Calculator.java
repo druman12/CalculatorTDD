27915c729365b10c87911166f807105514d15ada
Druman Kaneriya <116833054+druman12@users.noreply.github.com>
2025-07-03 23:22:55 +0530

    test 7 : passed

--- src/main/java/org/calculator/Calculator.java
+++ src/main/java/org/calculator/Calculator.java
@@ -5,7 +5,17 @@ public class Calculator {
         if(numbers.isEmpty()){
             return 0;
         }
-        String[] nums = numbers.split(",|\n");
+        // if there is not specified delimiter then
+        String delimiter = ",|\n";
+        // specified delimiter always start with //
+        if (numbers.startsWith("//")) {
+            // extract the customer delimiter
+            int index = numbers.indexOf("\n");
+            delimiter = java.util.regex.Pattern.quote(numbers.substring(2, index));
+
+            numbers = numbers.substring(index + 1); // new number string after the delimiter
+        }
+        String[] nums = numbers.split(delimiter); // split the number with updated delimiter
         int sum = 0;
         for (String num : nums) {
             String trimmed = num.trim();
